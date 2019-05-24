3ca076c8d40660c85b9b5c73beeb57cd7d6c1009
PIYUSH GUPTA <piyush.gupta111019@gmail.com>
2019-05-24 18:38:34 +0530

    rn integration

---
+++ example/App.js
@@ -0,0 +1,56 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import Animated from 'react-native-reanimated';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+
+    const a = 3 ; 
+   
+    const b = ` I have ${a} icecreams`;
+
+  
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}></Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>{instructions}</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
---
+++ example/babel.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  plugins: ['../lib/plugin.js'],
+  presets: ['module:metro-react-native-babel-preset'],
+};
